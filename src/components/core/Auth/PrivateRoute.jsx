9c3258384b81162790f7b22e3567b653faa82980
AshutoshDhumal11 <ashutoshcode04@gmail.com>
2025-10-05 19:45:54 +0530

    Define public and private routes

---
+++ src/components/core/Auth/PrivateRoute.jsx
@@ -0,0 +1,19 @@
+// non-authenticate users cannot access this routes
+
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+
+function PrivateRoute({ children }) {
+
+    const token = useSelector((state) => state.auth);
+
+    if(token !== null) {
+        return children
+    }
+    else {
+        return <Navigate to="/login"/>
+    }
+}
+
+export default PrivateRoute;
